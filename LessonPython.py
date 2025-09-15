684929501f597799902c9bf7066f0b99ca85b4f6
Francesco Fusiello <ffusiello22@gmail.com>
2025-09-15 19:39:00 +0200

    Lesson-3 Data types and variables

---
+++ LessonPython.py
@@ -0,0 +1,16 @@
+# Tipi di dati e variabili
+
+a = 12 #int
+b = 12.7 #float
+c = "Hello" #string
+c_bis ="17" #string
+c_ter = '17.24' #string
+d = True #boolean
+e = False #boolean
+
+print(type(d)) #<class 'bool'>
+print(type(str(d))) #<class 'str'>
+print(int(d)) #1
+print(int(e)) #0
+print(float(e)) #0.0
+print(float(a)) #12.0
